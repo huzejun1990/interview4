50c98792e42427c31b834a140baeef3b843abad9
huzejun <961639130@qq.com>
2024-07-22 21:33:49 +0800

    大厂面试题第4季 == 和 equals对比（jak17 对应的最低为lombok-1.18.24）2024-7-22 21:33:40

---
+++ src/com/dream/interview4/utils/Person.java
@@ -0,0 +1,24 @@
+package com.dream.interview4.utils;
+
+
+import lombok.*;
+
+/**
+ * @Author : huzejun
+ * @Date: 2024/7/21-23:11
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+    //温馨提醒，本类没有覆写hashcode和equals方法
+    private Integer id;
+    private String personName;
+
+    public Person(String personName)
+    {
+        this.personName = personName;
+    }
+}
